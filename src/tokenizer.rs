463a67719c653d447a835bb939512ac9d937dae1
MatricalDefunkt <pramitsmusic@gmail.com>
2023-11-29 05:26:58 +0530

    Refactor tokenizer to include additional punctuation
    Changed order of tokenization from punctuation -> keyword to keyword -> punctuation

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,7 +1,5 @@
 use std::char;
 
-use regex::Regex;
-
 use crate::utilities::{is_float, is_int, is_var};
 
 #[derive(Debug, PartialEq, Clone)]
@@ -19,7 +17,7 @@ pub struct VarData {
     pub var_value: String,
 }
 
-const PUNCTUATIONS: [char; 9] = [';', ':', ',', '(', ')', '{', '}', '[', ']'];
+pub const PUNCTUATIONS: [char; 10] = [';', ':', ',', '(', ')', '{', '}', '[', ']', '"'];
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum PunctuationType {
@@ -110,110 +108,19 @@ impl Tokenizer {
         let mut buf = String::new();
 
         while self.peek() != '\0' {
-            if self.current_char == '\n' || self.current_char == ';' {
-                self.next_line();
-            }
-
-            let mut found_string = false;
-
-            while !self.current_char.is_whitespace() {
-                if PUNCTUATIONS.contains(&self.current_char) {
-                    match self.current_char {
-                        ';' => self.tokens.push(Tokens::Punctuation {
-                            value: PunctuationType::Semicolon,
-                        }),
-                        ':' => self.tokens.push(Tokens::Punctuation {
-                            value: PunctuationType::Colon,
-                        }),
-                        ',' => self.tokens.push(Tokens::Punctuation {
-                            value: PunctuationType::Comma,
-                        }),
-                        '(' => self.tokens.push(Tokens::Punctuation {
-                            value: PunctuationType::LeftParen,
-                        }),
-                        ')' => self.tokens.push(Tokens::Punctuation {
-                            value: PunctuationType::RightParen,
-                        }),
-                        // '{' => tokens.push(Tokens::Punctuation {
-                        //     value: PunctuationType::LeftBrace,
-                        // }),
-                        // '}' => tokens.push(Tokens::Punctuation {
-                        //     value: PunctuationType::RightBrace,
-                        // }),
-                        // '[' => tokens.push(Tokens::Punctuation {
-                        //     value: PunctuationType::LeftBracket,
-                        // }),
-                        // ']' => tokens.push(Tokens::Punctuation {
-                        //     value: PunctuationType::RightBracket,
-                        // }),
-                        '"' => {
-                            self.tokens.push(Tokens::Punctuation {
-                                value: PunctuationType::DoubleQuote,
-                            });
-                            self.next_char();
-                            while self.current_char != '"' {
-                                buf.push(self.current_char);
-                                self.next_char();
-
-                                if self.current_char == '\\' {
-                                    self.next_char();
-                                    match self.current_char {
-                                        'n' => buf.push('\n'),
-                                        't' => buf.push('\t'),
-                                        'r' => buf.push('\r'),
-                                        '\\' => buf.push('\\'),
-                                        '"' => buf.push('"'),
-                                        '\'' => buf.push('\''),
-                                        _ => panic!(
-                                            "Unknown escape character: {}",
-                                            self.current_char
-                                        ),
-                                    }
-                                }
-                            }
-                            self.tokens
-                                .push(Tokens::StringLiteral { value: buf.clone() });
-                            self.tokens.push(Tokens::Punctuation {
-                                value: PunctuationType::DoubleQuote,
-                            });
-                            buf.clear();
-                            self.next_char();
-                            found_string = true;
-                            break;
-                        }
-                        _ => panic!("Unknown punctuation: {}", self.current_char),
-                    }
-                    self.next_char();
-                    break;
-                }
+            while !self.current_char.is_whitespace() && !PUNCTUATIONS.contains(&self.current_char) {
                 buf.push(self.current_char);
                 self.next_char();
             }
 
-            if found_string {
-                continue;
-            }
-
-            if buf.is_empty() {
+            if buf.is_empty() && !PUNCTUATIONS.contains(&self.current_char) {
+                self.next_char();
                 continue;
             };
 
             if KEYWORDS.contains(&buf.as_str()) {
                 match buf.as_str() {
-                    "let" => {
-                        self.tokens.insert(self.tokens.len() - 1, Tokens::Let);
-
-                        let mut data_type = String::new();
-
-                        while !self.current_char.is_whitespace()
-                            || PUNCTUATIONS.contains(&self.current_char)
-                        {
-                            data_type.push(self.current_char);
-                            self.next_char();
-                        }
-
-                        self.tokens.push(Tokens::Identifier { name: data_type });
-                    }
+                    "let" => self.tokens.push(Tokens::Let),
                     "exit" => self.tokens.push(Tokens::Exit),
                     "print" => self.tokens.push(Tokens::Print),
                     "add" => self.tokens.push(Tokens::Add),
@@ -234,12 +141,71 @@ impl Tokenizer {
                 });
             } else if is_var(&buf) {
                 self.tokens.push(Tokens::Identifier { name: buf.clone() });
-            } else if buf.is_empty() {
-                continue;
-            } else {
-                panic!("Unknown token: {}", buf);
             }
 
+            if PUNCTUATIONS.contains(&self.current_char) {
+                match self.current_char {
+                    ';' => {
+                        self.tokens.push(Tokens::Punctuation {
+                            value: PunctuationType::Semicolon,
+                        });
+                    }
+                    ':' => self.tokens.push(Tokens::Punctuation {
+                        value: PunctuationType::Colon,
+                    }),
+                    ',' => self.tokens.push(Tokens::Punctuation {
+                        value: PunctuationType::Comma,
+                    }),
+                    '(' => self.tokens.push(Tokens::Punctuation {
+                        value: PunctuationType::LeftParen,
+                    }),
+                    ')' => self.tokens.push(Tokens::Punctuation {
+                        value: PunctuationType::RightParen,
+                    }),
+                    // '{' => tokens.push(Tokens::Punctuation {
+                    //     value: PunctuationType::LeftBrace,
+                    // }),
+                    // '}' => tokens.push(Tokens::Punctuation {
+                    //     value: PunctuationType::RightBrace,
+                    // }),
+                    // '[' => tokens.push(Tokens::Punctuation {
+                    //     value: PunctuationType::LeftBracket,
+                    // }),
+                    // ']' => tokens.push(Tokens::Punctuation {
+                    //     value: PunctuationType::RightBracket,
+                    // }),
+                    '"' => {
+                        self.tokens.push(Tokens::Punctuation {
+                            value: PunctuationType::DoubleQuote,
+                        });
+                        self.next_char();
+                        while self.current_char != '"' {
+                            buf.push(self.current_char);
+                            self.next_char();
+
+                            if self.current_char == '\\' {
+                                self.next_char();
+                                match self.current_char {
+                                    'n' => buf.push('\n'),
+                                    't' => buf.push('\t'),
+                                    'r' => buf.push('\r'),
+                                    '\\' => buf.push('\\'),
+                                    '"' => buf.push('"'),
+                                    '\'' => buf.push('\''),
+                                    _ => panic!("Unknown escape character: {}", self.current_char),
+                                }
+                            }
+                        }
+                        self.tokens
+                            .push(Tokens::StringLiteral { value: buf.clone() });
+                        self.tokens.push(Tokens::Punctuation {
+                            value: PunctuationType::DoubleQuote,
+                        });
+                        buf.clear();
+                    }
+                    _ => panic!("Unknown punctuation: {}", self.current_char),
+                }
+            }
             self.next_char();
             buf.clear();
         }
